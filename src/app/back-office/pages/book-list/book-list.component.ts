d7daa89c30ccf4a96a7af1864e2a83b808b07098
YuriiBoltukov <kolomiha898@gmail.com>
2023-12-17 13:50:16 +0400

    add fab button

--- src/app/back-office/pages/book-list/book-list.component.ts
+++ src/app/back-office/pages/book-list/book-list.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss'],
-  imports: [IonicModule],
+  imports: [IonicModule, AsyncPipe],
 })
-export class BookListComponent {}
+export class BookListComponent {
+  addBook() {}
+}
